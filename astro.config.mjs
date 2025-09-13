27de6b2e754a52ad16b32e3b568801c0670c9e41
Óscar Bustos <oscarbustosgarrido@gmail.com>
2025-09-13 07:33:02 +0200

    chore: update site title, description and domain to Faedo de Ciñera

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://faedodecinera.example',
+  site: 'https://faedo.es',
   integrations: [
     mdx(),
     tailwind({
